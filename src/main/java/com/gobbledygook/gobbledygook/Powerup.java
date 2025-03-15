9247292bd7f7e74340158fcc6c075b1ce9837ac2
WilliamMorales1 <145170352+WilliamMorales1@users.noreply.github.com>
2025-03-15 18:34:19 -0500

    Update Powerup.java

--- src/main/java/com/gobbledygook/gobbledygook/Powerup.java
+++ src/main/java/com/gobbledygook/gobbledygook/Powerup.java
@@ -7,11 +7,11 @@ import java.util.UUID;
 
 @Data
 abstract class PowerUp {
-    protected UUID ownerOfPowerup;
+    protected Player ownerOfPowerup;
     protected String powerUpName;
     protected int numberOfUsesLeft;
 
-    public PowerUp(UUID ownerOfPowerup, String powerUpName, int numberOfUsesLeft) {
+    public PowerUp(Player ownerOfPowerup, String powerUpName, int numberOfUsesLeft) {
         this.ownerOfPowerup = ownerOfPowerup;
         this.powerUpName = powerUpName;
         this.numberOfUsesLeft = numberOfUsesLeft;
