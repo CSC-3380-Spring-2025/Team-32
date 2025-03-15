4bbf2f67cde42c9b777c8975d229fe485c927f75
WilliamMorales1 <145170352+WilliamMorales1@users.noreply.github.com>
2025-03-15 18:54:01 -0500

    Update DoubleOrNothing.java

--- src/main/java/com/gobbledygook/gobbledygook/DoubleOrNothing.java
+++ src/main/java/com/gobbledygook/gobbledygook/DoubleOrNothing.java
@@ -1,21 +1,41 @@
 package com.gobbledygook.gobbledygook;
 
 import lombok.Data;
-import java.util.UUID;
 
 @Data
-class DoubleOrNothing extends PowerUp {
-    public DoubleOrNothing(UUID ownerOfPowerup) {
-        super(ownerOfPowerup, "Double or Nothing", 3); // Initialize base class fields
+public class DoubleOrNothing extends PowerUp {
+    private Round targetRound; // Store the round when the power-up is activated
+
+    public DoubleOrNothing(Player ownerOfPowerup) {
+        super(ownerOfPowerup, "Double or Nothing", 3);
     }
 
     @Override
-    void usePowerup() {
+    public void usePowerup(GameSession gameSession, Player user) {
         if (numberOfUsesLeft > 0) {
             numberOfUsesLeft--;
-            System.out.println("Used Double or Nothing! Uses left: " + numberOfUsesLeft);
+
+            targetRound = gameSession.getCurrentRound(); // Store the current round
+
+            // uncomment if messege needed 
+            // System.out.println(user.getName() + " activated Double or Nothing for Round " + targetRound.getRoundNumber());
         } else {
             System.out.println("No uses left for Double or Nothing!");
+        }
+    }
+
+    // is called whenever the game state is changed
+    public void onGameStateChange(GameSession gameSession, Player user) {
+        if (targetRound != null && gameSession.getCurrentRound() == targetRound
+            && gameSession.getState() == GameState.ROUND_END) {
+
+            user.setScore(user.getScore() * 2);
+            System.out.println(user.getName() + "'s score doubled with double or nothing!" // regular messege
+            // messege for debugging 
+            // System.out.println(user.getName() + "'s score doubled to " + user.getScore() + " at the end of Round " + targetRound.getRoundNumber());
+
+            // Reset targetRound after applying the effect
+            targetRound = null;
         }
     }
 }
