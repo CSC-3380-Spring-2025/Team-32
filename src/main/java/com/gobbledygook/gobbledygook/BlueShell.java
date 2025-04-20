11548a0a7f7c79c3e5564f0fb8ffed746e074582
jmadripz <jmadri2@lsu.edu>
2025-04-20 12:06:38 -0500

    Fixed compiler errors for gameSession and BlueShell (#89)
    * fixing name pt1
    
    * fixing name pt2
    
    * fixed compiler errors (blushell,gamesession)

--- src/main/java/com/gobbledygook/gobbledygook/BlueShell.java
+++ src/main/java/com/gobbledygook/gobbledygook/BlueShell.java
@@ -8,17 +8,18 @@ public class BlueShell extends PowerUp {
         super(ownerOfPowerup, "Blue Shell", 3);
     }
 
-    public void usePowerup() {
+    public boolean usePowerup() {
         if (numberOfUsesLeft > 0) {
             numberOfUsesLeft--;
-
-            topPlayer = gameSession.getTopPlayer(); // get top player
-            topPlayer.score = topPlayer.score - 10; // arbitrarily set to -10, can change if needed
-
+            
+            //Player topPlayer = gameSession.getTopPlayer(); // get top player
+            //topPlayer.setScore(topPlayer.getScore() - 10); // arbitrarily set to -10, can change if needed
+            return true;
             // uncomment if messege needed 
-            // System.out.println(user.getName() + " activated Double or Nothing for Round " + targetRound.getRoundNumber());
+            // System.out.println(user.getName() + " activated BlueShell " + targetRound.getRoundNumber());
         } else {
-            System.out.println("No uses left for Double or Nothing!");
+            System.out.println("No uses left for Blue Shell!");
+            return false;
         }
     }
 }
