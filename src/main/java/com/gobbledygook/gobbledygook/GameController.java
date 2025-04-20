620f7f44c015040f55735a35f830f6554448f0af
Joseph Strauss <92337469+cuppajoe123@users.noreply.github.com>
2025-04-20 12:17:10 -0500

    Fix definitions voting screen (#95)
    Co-authored-by: Calvin Gavin <cgavin7@lsu.edu>

--- src/main/java/com/gobbledygook/gobbledygook/GameController.java
+++ src/main/java/com/gobbledygook/gobbledygook/GameController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import java.util.Random;
 import java.util.UUID;
 
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
